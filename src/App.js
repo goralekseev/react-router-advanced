79970699cefb9198aa20e14336188cbd54589c92
Igor Alekseev <tauron19@gmail.com>
2022-05-05 01:42:09 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import AllQuotes from "./pages/AllQuotes";
+import NewQuotes from "./pages/NewQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route exact path="/quotes">
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quiteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="new-quote">
+          <NewQuotes />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
